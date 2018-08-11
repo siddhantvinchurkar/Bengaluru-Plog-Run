3de21f222b4a8ee9ffd439b7478841da8a903d7e
Vinchurkar <siddhantvinchurkar@gmail.com>
2018-08-11 14:21:16 +0530

    Finished ambassadors page

---
+++ manager/main.js
@@ -0,0 +1,219 @@
+// Global db object
+var gdb;
+
+window.onload = function(){
+
+	// Register a Service Worker
+	if('serviceWorker' in navigator) {
+	  navigator.serviceWorker
+	           .register('sw.js')
+	           .then(function() { console.log("%c Service Worker Registered", "background:#222222; color:#BADA55;"); });
+	}
+
+	// Set year on footer
+	document.getElementById("footerYear").innerHTML = new Date().getFullYear();
+
+	// Initialize Firebase
+	firebase.initializeApp({
+	apiKey: 'AIzaSyBgEsyiffCICWGf32cc4SspPa4MvKJf1Hw',
+	authDomain: 'bengaluru-plog-run.firebaseapp.com',
+	projectId: 'bengaluru-plog-run'
+	});
+
+	// Acknowledge changes
+	firebase.firestore().settings({timestampsInSnapshots: true});
+
+	// Initialize Cloud Firestore through Firebase
+	var db = firebase.firestore();
+
+	// Transfer scope
+	gdb = db;
+
+	// Password store
+	var pwd = "password";
+
+	// Retrieve password from the database
+	db.collection("passwords").get().then((querySnapshot) => {
+		querySnapshot.forEach((doc) => {
+			pwd = doc.data().pwd;
+			// Enable signin
+			document.getElementById("password").disabled = false;
+			document.getElementById("passwordProgress").style.display = "none";
+			document.getElementById("passwordField").style.display = "block";
+			document.getElementById("passwordButton").style.display = "block";
+			document.getElementById("passwordMessage").style.display = "block";
+
+			// Begin fetching volunteer and/or ambassador records in the background
+			db.collection("volunteers").get().then((querySnapshot) => {
+				querySnapshot.forEach((doc) => {
+					// Background fetch complete; hide progress bar
+					document.getElementById("tableProgress").style.display = "none";
+					buildTableRow(doc.data().firstName + " " + doc.data().lastName, doc.data().email, doc.data().designation);
+				});
+			});
+		});
+	});
+
+	// Handle Sign in
+	document.getElementById("passwordButton").onclick = function(){
+		if(document.getElementById("password").value === pwd){
+			// Password is correct; grant access
+			document.getElementById("signInCard").style.display = "none";
+			document.getElementById("records").style.display = "block";
+		}
+		else{
+			// Password is wrong; deny access
+			alert("Nope. Try again.");
+		}
+	}
+
+	// Handle enter keypress
+	document.getElementById("password").addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if(event.keyCode === 13){
+			document.getElementById("passwordButton").click();
+		}
+	});
+
+	// Handle 'Spacebar' keypress
+	document.addEventListener("keydown", function(event) {
+		if(event.keyCode === 32){
+			// Refresh table
+			document.getElementById("tableContents").innerHTML = "";
+			document.getElementById("tableProgress").style.display = "block";
+			db.collection("volunteers").get().then((querySnapshot) => {
+				querySnapshot.forEach((doc) => {
+					// Background fetch complete; hide progress bar
+					document.getElementById("tableProgress").style.display = "none";
+					buildTableRow(doc.data().firstName + " " + doc.data().lastName, doc.data().email, doc.data().designation);
+				});
+					// Scroll table into view
+				document.getElementById("records").scrollIntoView();
+				});
+		}
+	});
+
+	// Handle refresh
+	document.getElementById("refreshButton").onclick = function(){
+			// Refresh table
+		document.getElementById("tableContents").innerHTML = "";
+		document.getElementById("tableProgress").style.display = "block";
+		db.collection("volunteers").get().then((querySnapshot) => {
+			querySnapshot.forEach((doc) => {
+				// Background fetch complete; hide progress bar
+				document.getElementById("tableProgress").style.display = "none";
+				buildTableRow(doc.data().firstName + " " + doc.data().lastName, doc.data().email, doc.data().designation);
+			});
+
+			// Scroll table into view
+			document.getElementById("records").scrollIntoView();
+
+		});
+
+	}
+
+}
+
+// Handle button hover
+function startPulse(button){
+	button.classList.add("pulse");
+}
+
+function stopPulse(button){
+	button.classList.remove("pulse");
+}
+
+// Build table rows
+function buildTableRow(name="unknown", email="unknown", designation="unknown"){
+
+	// Flags
+	var ambassador = false;
+
+	// Handle single quotes
+	name = name.replace(/'/g, "\\'");
+	email = email.replace(/'/g, "\\'");
+	designation = designation.replace(/'/g, "\\'");
+
+	// Handle designation styling
+	if(designation === "volunteer") designation = '<td style="color:#FFD700;">Volunteer</td>';
+	else {designation = '<td style="color:#FF0000;">Ambassador</td>'; ambassador = true;}
+
+	// Update table contents
+	if(ambassador)
+		document.getElementById("tableContents").innerHTML += '<tr><td>'+name+'</td><td>'+email+'</td>'+designation+'<td><a href="#someemailservice" onmouseover="startPulse(this);" onmouseout="stopPulse(this);" onclick="downgradeAmbassador(\''+name+'\', \''+email+'\')" class="btn-floating" style="background-color:#AA0000;" title="Downgrade '+name+' to Volunteer"><i class="material-icons">arrow_downward</i></a></td></tr>';
+	else
+		document.getElementById("tableContents").innerHTML += '<tr><td>'+name+'</td><td>'+email+'</td>'+designation+'<td><a href="#someemailservice" onmouseover="startPulse(this);" onmouseout="stopPulse(this);" onclick="upgradeVolunteer(\''+name+'\', \''+email+'\')" class="btn-floating" title="Upgrade '+name+' to Ambassador"><i class="material-icons">arrow_upward</i></a></td></tr>';
+
+}
+
+// Handle upgrades and downgrades
+function downgradeAmbassador(name, email){
+
+	// Start Work
+	document.getElementById("tableProgress").style.display = "block";
+
+	// Update Firestore document
+	gdb.collection("volunteers").doc(email).update({
+		designation: "volunteer"
+	})
+	.then(function(docRef) {
+
+		// Refresh table
+		document.getElementById("tableContents").innerHTML = "";
+		document.getElementById("tableProgress").style.display = "block";
+		gdb.collection("volunteers").get().then((querySnapshot) => {
+			querySnapshot.forEach((doc) => {
+				// Background fetch complete; hide progress bar
+				document.getElementById("tableProgress").style.display = "none";
+				buildTableRow(doc.data().firstName + " " + doc.data().lastName, doc.data().email, doc.data().designation);
+			});
+
+			// Display toast
+			M.toast({html: name + ' is now a volunteer!', classes: 'rounded'});
+
+			// Scroll table into view
+			document.getElementById("records").scrollIntoView();
+
+			});
+		})
+		.catch(function(error) {
+			console.error("Error adding document: ", error);
+		});
+
+	}
+
+function upgradeVolunteer(name, email){
+
+	// Start Work
+	document.getElementById("tableProgress").style.display = "block";
+
+	// Update Firestore document
+	gdb.collection("volunteers").doc(email).update({
+		designation: "ambassador"
+	})
+	.then(function(docRef) {
+
+		// Refresh table
+		document.getElementById("tableContents").innerHTML = "";
+		document.getElementById("tableProgress").style.display = "block";
+		gdb.collection("volunteers").get().then((querySnapshot) => {
+			querySnapshot.forEach((doc) => {
+				// Background fetch complete; hide progress bar
+				document.getElementById("tableProgress").style.display = "none";
+				buildTableRow(doc.data().firstName + " " + doc.data().lastName, doc.data().email, doc.data().designation);
+			});
+
+			// Display toast
+			M.toast({html: name + ' is now an ambassador!', classes: 'rounded'});
+
+			// Scroll table into view
+			document.getElementById("records").scrollIntoView();
+
+		});
+
+	})
+	.catch(function(error) {
+		console.error("Error adding document: ", error);
+	});
+
+}
