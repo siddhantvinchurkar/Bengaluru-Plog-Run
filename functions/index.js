eb76d97d7e8dc17b49ea35d1f5d67ed01a57ebda
Vinchurkar <siddhantvinchurkar@gmail.com>
2018-08-13 21:38:35 +0530

    Modified some Firebase Function

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,6 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.server-status = functions.https.onRequest((request, response) => {
-	response.send("The server is fully operational. All services are running as expected.");
-});
-exports.hello-world = functions.https.onRequest((request, response) => {
+exports.helloWorld = functions.https.onRequest((request, response) => {
 	response.send("Hello from <b>Sniper Angel</b>!");
 });
