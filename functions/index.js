2637cb4bf1e88f28aeb3763a4e7b5b275f01776d
Vinchurkar <siddhantvinchurkar@gmail.com>
2018-08-13 21:29:01 +0530

    Added two Firebase Functions

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,9 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	response.send("Hello from Firebase!");
+exports.server-status = functions.https.onRequest((request, response) => {
+	response.send("The server is fully operational. All services are running as expected.");
+});
+exports.hello-world = functions.https.onRequest((request, response) => {
+	response.send("Hello from <b>Sniper Angel</b>!");
 });
