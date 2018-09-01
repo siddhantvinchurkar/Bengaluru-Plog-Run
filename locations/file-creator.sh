f78a5b7a2cfe5dfdfcee8a0463a0fe61f55aa748
Vinchurkar <siddhantvinchurkar@gmail.com>
2018-09-01 20:53:14 +0530

    Added file creator script

---
+++ locations/file-creator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+touch  ADUGODI.json
+touch ANJANAPURA.json
+touch ARAKERE.json
+touch ATTIGUPPE.json
+touch BASAVESHWARANAGAR.json
+touch BASAVANAGUDI.json
+touch BTM_LAYOUT.json
+touch BANASHANKARI_2ND_STAGE.json
+touch BYRASANDRA.json
+touch CHAMRAJPET.json
